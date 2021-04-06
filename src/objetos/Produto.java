40f0ccff821e72aae921017900b03f2e63094b2a
GideoneClementino <gideonedeon@hotmail.com>
2021-04-06 00:04:37 -0300

    Adicionado construtor ao objeto Produto e reestruturando a classe Programa_produto.

--- src/objetos/Produto.java
+++ src/objetos/Produto.java
@@ -1,29 +1,35 @@
 package objetos;
 
 public class Produto {
-	public String nome;
-	public double preco;
-	public int quantidade;
-	
-	public double valorTotalNoEstoque() {
-		return preco * quantidade;
-	}
-	
-	public void adicionarProdutos(int quantidade) {
-		this.quantidade += quantidade;
-	}
-	
-	public void removerProdutos(int quantidade) {
-		this.quantidade -= quantidade;
-	}
-	
-	public String toString() {
-		return nome
-			+ ", $ "
-			+ String.format("%.2f", preco)
-			+ ", "
-			+ quantidade
-			+ " unidades, Total: $ "
-			+ String.format("%.2f", valorTotalNoEstoque());
-	}
+    public String nome;
+    public double preco;
+    public int quantidade;
+
+    public Produto(String nome, double preco, int quantidade) {
+        this.nome = nome;
+        this.preco = preco;
+        this.quantidade = quantidade;
+    }
+
+    public double valorTotalNoEstoque() {
+        return preco * quantidade;
+    }
+
+    public void adicionarProdutos(int quantidade) {
+        this.quantidade += quantidade;
+    }
+
+    public void removerProdutos(int quantidade) {
+        this.quantidade -= quantidade;
+    }
+
+    public String toString() {
+        return nome
+                + ", $ "
+                + String.format("%.2f", preco)
+                + ", "
+                + quantidade
+                + " unidades, Total: $ "
+                + String.format("%.2f", valorTotalNoEstoque());
+    }
 }
--- src/poo1/Programa_produto.java
+++ src/poo1/Programa_produto.java
@@ -7,41 +7,41 @@ import objetos.Produto;
 
 public class Programa_produto {
 
-	public static void main(String[] args) {
-		Locale.setDefault(Locale.US);
-		Scanner sc = new Scanner(System.in);
-		
-		Produto produto = new Produto();
-		
-		System.out.println("Digite os dados do produto: ");
-		System.out.print("Nome: ");
-		produto.nome = sc.nextLine();
-		System.out.print("Preço: ");
-		produto.preco = sc.nextDouble();
-		System.out.println("Quantidade no estoque: ");
-		produto.quantidade = sc.nextInt();
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
 
-		System.out.println();
-		
-		System.out.println("Dados do produto: " + produto.toString());
-		
-		System.out.println();
-		System.out.println("Digite o número de produtos para adicionar no estoque: ");
-		int quantidade = sc.nextInt();
-		produto.adicionarProdutos(quantidade);
-		
-		System.out.println();
-		System.out.println("Dados atualizados: " + produto.toString());
-		
-		System.out.println();
-		System.out.println("Digite o número de produtos para remover do estoque: ");
-		quantidade = sc.nextInt();
-		produto.removerProdutos(quantidade);
-		
-		System.out.println();
-		System.out.println("Dados atualizados: " + produto.toString());
-		
-		sc.close();
-	}
+        System.out.println("Digite os dados do produto: ");
+        System.out.print("Nome: ");
+        String nome = sc.nextLine();
+        System.out.print("PreÃ§o: ");
+        double preco = sc.nextDouble();
+        System.out.println("Quantidade no estoque: ");
+        int quantidade = sc.nextInt();
+
+        Produto produto = new Produto(nome, preco, quantidade);
+
+        System.out.println();
+
+        System.out.println("Dados do produto: " + produto.toString());
+
+        System.out.println();
+        System.out.println("Digite o nÃºmero de produtos para adicionar no estoque: ");
+        quantidade = sc.nextInt();
+        produto.adicionarProdutos(quantidade);
+
+        System.out.println();
+        System.out.println("Dados atualizados: " + produto.toString());
+
+        System.out.println();
+        System.out.println("Digite o nÃºmero de produtos para remover do estoque: ");
+        quantidade = sc.nextInt();
+        produto.removerProdutos(quantidade);
+
+        System.out.println();
+        System.out.println("Dados atualizados: " + produto.toString());
+
+        sc.close();
+    }
 
 }
